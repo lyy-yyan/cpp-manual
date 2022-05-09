76a7dc70f2c460a95c18e9bb33934945c3cdbeaa
lyy_yyan <lyy_yyan@163.com>
2022-05-09 22:21:54 +0800

    Study for constructor.

---
+++ com/lyy/oppBase1/complex.h
@@ -0,0 +1,227 @@
+#ifndef __MYCOMPLEX__   // 防卫式声明：如果没有声明过，则继续（可防止重复声明），使用#endif结束
+#define __MYCOMPLEX__   // 声明
+
+// 前置声明
+class complex; 
+complex&
+  __doapl (complex* ths, const complex& r);
+complex&
+  __doami (complex* ths, const complex& r);
+complex&
+  __doaml (complex* ths, const complex& r);
+
+// 类的声明
+class complex
+{
+public:
+  // 初始化行为，若将re = r; im = i;写入{}中，则时赋值行为，效率比初始化行为低
+  // 这种写法仅限构造函数 ~: re (r), im (i)
+  complex (double r = 0, double i = 0): re (r), im (i) { }
+  complex& operator += (const complex&);
+  complex& operator -= (const complex&);
+  complex& operator *= (const complex&);
+  complex& operator /= (const complex&);
+  // 即使是写在body内部的函数也不一定被编译为inline函数，太复杂的函数就不会
+  double real () const { return re; }
+  double imag () const { return im; }
+private:
+    double re, im;
+    // 可以使用模板声明就可在创建对象时灵活使用数据类型 T re, im; complex<int> c2(1, 6);
+
+  friend complex& __doapl (complex *, const complex&);
+  friend complex& __doami (complex *, const complex&);
+  friend complex& __doaml (complex *, const complex&);
+};
+
+
+// 类的定义
+// inline指内敛函数，要求编译器尽量将该函数编译为内敛函数，仅是尽量
+inline complex&
+__doapl (complex* ths, const complex& r)
+{
+  ths->re += r.re;
+  ths->im += r.im;
+  return *ths;
+}
+ 
+inline complex&
+complex::operator += (const complex& r)
+{
+  return __doapl (this, r);
+}
+
+inline complex&
+__doami (complex* ths, const complex& r)
+{
+  ths->re -= r.re;
+  ths->im -= r.im;
+  return *ths;
+}
+ 
+inline complex&
+complex::operator -= (const complex& r)
+{
+  return __doami (this, r);
+}
+ 
+inline complex&
+__doaml (complex* ths, const complex& r)
+{
+  double f = ths->re * r.re - ths->im * r.im;
+  ths->im = ths->re * r.im + ths->im * r.re;
+  ths->re = f;
+  return *ths;
+}
+
+inline complex&
+complex::operator *= (const complex& r)
+{
+  return __doaml (this, r);
+}
+ 
+inline double
+imag (const complex& x)
+{
+  return x.imag ();
+}
+
+inline double
+real (const complex& x)
+{
+  return x.real ();
+}
+
+inline complex
+operator + (const complex& x, const complex& y)
+{
+  return complex (real (x) + real (y), imag (x) + imag (y));
+}
+
+inline complex
+operator + (const complex& x, double y)
+{
+  return complex (real (x) + y, imag (x));
+}
+
+inline complex
+operator + (double x, const complex& y)
+{
+  return complex (x + real (y), imag (y));
+}
+
+inline complex
+operator - (const complex& x, const complex& y)
+{
+  return complex (real (x) - real (y), imag (x) - imag (y));
+}
+
+inline complex
+operator - (const complex& x, double y)
+{
+  return complex (real (x) - y, imag (x));
+}
+
+inline complex
+operator - (double x, const complex& y)
+{
+  return complex (x - real (y), - imag (y));
+}
+
+inline complex
+operator * (const complex& x, const complex& y)
+{
+  return complex (real (x) * real (y) - imag (x) * imag (y),
+			   real (x) * imag (y) + imag (x) * real (y));
+}
+
+inline complex
+operator * (const complex& x, double y)
+{
+  return complex (real (x) * y, imag (x) * y);
+}
+
+inline complex
+operator * (double x, const complex& y)
+{
+  return complex (x * real (y), x * imag (y));
+}
+
+complex
+operator / (const complex& x, double y)
+{
+  return complex (real (x) / y, imag (x) / y);
+}
+
+inline complex
+operator + (const complex& x)
+{
+  return x;
+}
+
+inline complex
+operator - (const complex& x)
+{
+  return complex (-real (x), -imag (x));
+}
+
+inline bool
+operator == (const complex& x, const complex& y)
+{
+  return real (x) == real (y) && imag (x) == imag (y);
+}
+
+inline bool
+operator == (const complex& x, double y)
+{
+  return real (x) == y && imag (x) == 0;
+}
+
+inline bool
+operator == (double x, const complex& y)
+{
+  return x == real (y) && imag (y) == 0;
+}
+
+inline bool
+operator != (const complex& x, const complex& y)
+{
+  return real (x) != real (y) || imag (x) != imag (y);
+}
+
+inline bool
+operator != (const complex& x, double y)
+{
+  return real (x) != y || imag (x) != 0;
+}
+
+inline bool
+operator != (double x, const complex& y)
+{
+  return x != real (y) || imag (y) != 0;
+}
+
+#include <cmath>
+
+inline complex
+polar (double r, double t)
+{
+  return complex (r * cos (t), r * sin (t));
+}
+
+inline complex
+conj (const complex& x) 
+{
+  return complex (real (x), -imag (x));
+}
+
+inline double
+norm (const complex& x)
+{
+  return real (x) * real (x) + imag (x) * imag (x);
+}
+
+#endif   //__MYCOMPLEX__
+
+
+
+
