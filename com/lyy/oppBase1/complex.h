77578c9daa4e3b3fdfbef57e0753fb93977a0610
lyy_yyan <lyy_yyan@163.com>
2022-05-13 00:28:12 +0800

    Operator overloading learning.

--- com/lyy/oppBase1/complex.h
+++ com/lyy/oppBase1/complex.h
@@ -18,8 +18,10 @@ public:
   // 这种写法仅限构造函数 ~: re (r), im (i)
   // 下面构造函数的参数传递为值传递，如果可以使用引用传递，请尽量使用，效率更高
   complex (double r = 0, double i = 0): re (r), im (i) { }
-  complex& operator += (const complex&);  // 参数为引用传递
-  complex& operator -= (const complex&);
+  
+  // 定义在class内的函数为成员函数，在其外的为全局函数
+  complex& operator += (const complex&);      // 参数为引用传递
+  complex& operator -= (const complex&);      // 操作符重载
   complex& operator *= (const complex&);
   complex& operator /= (const complex&);
   // 即使是写在body内部的函数也不一定被编译为inline函数，太复杂的函数就不会
@@ -40,18 +42,18 @@ private:
 
 // 类的定义
 // inline指内敛函数，要求编译器尽量将该函数编译为内敛函数，仅是尽量
-inline complex&
+inline complex&                             // 返回值以引用形式接收
 __doapl (complex* ths, const complex& r)
 {
   ths->re += r.re;
   ths->im += r.im;
-  return *ths;
+  return *ths;                              // 传递者以指针形式传递，传递者无需知道接收者是以引用形式接收的
 }
  
 inline complex&
-complex::operator += (const complex& r)
+complex::operator += (const complex& r)     // ::即成员函数未在类内部声明定义，在类外部定义
 {
-  return __doapl (this, r);
+  return __doapl (this, r);                 // 哪个对象调用这个函数，this就是那个对象
 }
 
 inline complex&
@@ -95,10 +97,10 @@ real (const complex& x)
   return x.real ();
 }
 
-inline complex
+inline complex                                                // 这里无法返回引用类型，因为函数返回的是一个局部变量
 operator + (const complex& x, const complex& y)
 {
-  return complex (real (x) + real (y), imag (x) + imag (y));
+  return complex (real (x) + real (y), imag (x) + imag (y));  // 创建一个临时对象complex
 }
 
 inline complex
@@ -156,7 +158,7 @@ operator / (const complex& x, double y)
   return complex (real (x) / y, imag (x) / y);
 }
 
-inline complex
+inline complex      // 此行可以修改为inline complex&，效率更高，此处的x并非局部变量，可用引用返回
 operator + (const complex& x)
 {
   return x;
@@ -202,6 +204,14 @@ inline bool
 operator != (double x, const complex& y)
 {
   return x != real (y) || imag (y) != 0;
+}
+
+// new add 可以重载输出操作符 <<
+# include <iostream>
+ostream& 
+operator << (ostream& os, const complex& x)
+{
+  return os << '(' << real(x) << ', ' << imag(x) << ')';
 }
 
 #include <cmath>
